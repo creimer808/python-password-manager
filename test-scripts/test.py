bcc07773ac00ee626361a33c61161eb2a1b23f9b
creimer808 <cameron.reimer@protonmail.com>
2023-12-01 10:28:17 -0600

    added functionality to test scripts.

--- test-scripts/test.py
+++ test-scripts/test.py
@@ -1,5 +1,6 @@
-from authentication import authentication
-
+import sqlite3
+def main():
+    list_columns()
 #first_name = "John"
 #last_name = "Doe"
 #email = "john.doe@example.com"
@@ -9,8 +10,24 @@ from authentication import authentication
 #register user:
 #authentication.register_user(first_name, last_name, email, hashed_password, salt)
 
-email = "john.doe@example.com"
-password = "Password1!"
+#email = "john.doe@example.com"
+#password = "Password1!"
 
-authentication.login_user(email,password)
+#authentication.login_user(email,password)
 
+### Connect to DB, list all columns in USERS table. ###
+def list_columns():    
+    conn = sqlite3.connect('secrets.db')
+    cursor = conn.cursor()
+        
+    cursor.execute("PRAGMA table_info(users)")
+    columns = cursor.fetchall()
+
+    for i in columns:
+        print(columns[1])
+
+    conn.commit()
+    conn.close()
+    
+if __name__ == "__main__":
+    main()
